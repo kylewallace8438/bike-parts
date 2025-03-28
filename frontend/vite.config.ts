8c05ac62fce48e2a110f3b0b9c20b19e9626e503
viethq <viethaquoc@zigexn.vn>
2025-03-28 16:57:25 +0700

    update gitignore

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,5 +12,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: '../public/app',
+    reportCompressedSize: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'app.js',
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].js'
+      },
+    },
+    emptyOutDir: true
+  },
 })
