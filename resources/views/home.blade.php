08fd7511fe21a3aa31954c5c19378648d139ef3f
Kyle <viet@beae.com>
2025-05-23 10:57:48 +0700

    update seo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kwapp Store | Phụ tùng và bảo dưỡng xe</title>
-    <meta name="description" content="Kwapp Store - Cửa hàng phụ tùng và bảo dưỡng xe máy KTM chất lượng. Các loại phụ tùng hao mòn, đồ chơi, dầu nhớt dành cho KTM và Husqvana luôn có sẵn. Gọi điện đặt lịch để được thay thế lắp đặt ngay.">
+    <title>Kwapp Store | Tìm kiếm phụ tùng và bảo dưỡng xe KTM tại Hà Nội</title>
+    <meta name="description" content="Kwapp Store - Cửa hàng phụ tùng và bảo dưỡng xe máy KTM tại Hà Nội. Các loại phụ tùng, đồ chơi, dầu nhớt dành cho KTM và Husqvana luôn có sẵn. Gọi điện đặt lịch để được thay thế hoặc lắp đặt ngay.">
     <meta name="keywords"
-        content="Kwapp Store, phụ tùng xe máy, bảo dưỡng xe, sửa chữa xe máy, đồ chơi xe máy, phụ kiện xe máy, dầu nhớt, lốp xe, phanh xe, nhông sên dĩa, ktm, husqvana, svartpilen, vitpilen, RC, Duke">
+        content="Kwapp Store, phụ tùng, bảo dưỡng xe, sửa chữa xe máy, đồ chơi xe máy, phụ kiện xe máy, dầu nhớt, lốp xe, phanh xe, nhông sên dĩa, ktm, husqvana, svartpilen, vitpilen, RC, Duke">
     <meta name="author" content="Kwapp Store">
     <meta name="robots" content="index,follow">
+
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://kwappstore.com">
-    <meta property="og:title" content="Kwapp Store | Phụ tùng và bảo dưỡng xe">
-    <meta property="og:description" content="Kwapp Store - Cửa hàng phụ tùng và bảo dưỡng xe máy chất lượng cao">
+    <meta property="og:title" content="Kwapp Store | Tìm kiếm phụ tùng và bảo dưỡng xe KTM tại Hà Nội">
+    <meta property="og:description" content="Kwapp Store - Cửa hàng phụ tùng và bảo dưỡng xe máy KTM tại Hà Nội. Các loại phụ tùng, đồ chơi, dầu nhớt dành cho KTM và Husqvana luôn có sẵn. Gọi điện đặt lịch để được thay thế hoặc lắp đặt ngay.">
+    <meta property="og:image" content="https://kwapp.store/images/og_image.png" />
+
     <meta rel="canonical" href="https://kwappstore.com">
     <link rel="preconnect" href="https://kwappstore.com" crossorigin>
     <link rel="dns-prefetch" href="https://fonts.google.com/">
