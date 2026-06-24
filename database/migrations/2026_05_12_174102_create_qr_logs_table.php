15219340d6dc81f22fe9de399159348bb99471da
Kyle <hqv1211@gmail.com>
2026-06-25 00:18:41 +0700

    updaate workflow

--- database/migrations/2026_05_12_174102_create_qr_logs_table.php
+++ database/migrations/2026_05_12_174102_create_qr_logs_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('qr_logs', function (Blueprint $table) {
+        Schema::create("qr_logs", function (Blueprint $table) {
             $table->uuid()->primary();
-            $table->unsignedBigInteger('maintenance_id');
-            $table->string('qr_path');
+            $table->unsignedBigInteger("maintenance_id");
+            $table->string("qr_path");
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('qr_logs');
+        Schema::dropIfExists("qr_logs");
     }
 };
