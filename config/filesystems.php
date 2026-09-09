71277ae23582e524fcfcba5636b61b356170685d
Kyle <viet.haquoc@powergatesoftware.com>
2026-09-09 11:33:27 +0700

    add Cloudflare R2

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,17 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'r2' => [
+            'driver' => 's3',
+            'key' => env('CLOUDFLARE_R2_ACCESS_KEY_ID'),
+            'secret' => env('CLOUDFLARE_R2_SECRET_ACCESS_KEY'),
+            'region' => 'us-east-1', // R2 ignores this, but Flysystem requires it
+            'bucket' => env('CLOUDFLARE_R2_BUCKET'),
+            'url' => env('CLOUDFLARE_R2_URL'),
+            'endpoint' => env('CLOUDFLARE_R2_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'throw' => true,
+        ],
 
     ],
 
